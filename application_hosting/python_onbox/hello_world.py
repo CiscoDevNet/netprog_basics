7e5f4466480a8fed8f5024a2686e769258329211
Hank Preston <hank.preston@gmail.com>
2017-10-02 14:36:23 -0400

    Hello World for On Box Python

---
+++ application_hosting/python_onbox/hello_world.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+"""
+Learning Series: Network Programmability Basics
+Module: Application Hosting
+Lesson: Python at the Edge
+Author: Hank Preston <hapresto@cisco.com>
+
+hello_world.py
+Illustrate the following concepts:
+- Run a Python script on-box on
+"""
+
+__author__ = "Hank Preston"
+__author_email__ = "hapresto@cisco.com"
+__copyright__ = "Copyright (c) 2016 Cisco Systems, Inc."
+__license__ = "MIT"
+
+print("Hello World!")
