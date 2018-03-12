9db3791aceda8e7f6677ea7375ba90f8e51da635
hpreston <hank.preston@gmail.com>
2018-03-12 14:24:56 -0400

    Updated to reflect Sandbox Changes

--- programming_fundamentals/python_part_3/api_ncclient_example.py
+++ programming_fundamentals/python_part_3/api_ncclient_example.py
@@ -1,4 +1,4 @@
-! /usr/bin/env python
+#! /usr/bin/env python
 """
 Learning Series: Network Programmability Basics
 Module: Programming Fundamentals
@@ -8,7 +8,7 @@ Author: Hank Preston <hapresto@cisco.com>
 api_ncclient_example.py
 Illustrate the following concepts:
 - Making NETCONF calls using ncclient library
-- Intended to be entered into an interactive 
+- Intended to be entered into an interactive
   interpreter
 """
 
@@ -16,11 +16,11 @@ from ncclient import manager
 from pprint import pprint
 import xmltodict
 
-router = {"ip": "10.10.20.21", 
-          "port": 830, 
-          "user": "root", 
-          "pass": "cisco123"}
-          
+router = {"ip": "10.10.20.48",
+          "port": 830,
+          "user": "cisco",
+          "pass": "cisco_1234!"}
+
 netconf_filter = """
 <filter>
   <interfaces xmlns="urn:ietf:params:xml:ns:yang:ietf-interfaces">
@@ -29,15 +29,14 @@ netconf_filter = """
     </interface>
   </interfaces>
 </filter>
-"""      
+"""
 
-m = manager.connect(host=router["ip"], 
-                    port=router["port"], 
-                    username=router["user"], 
-                    password=router["pass"], 
+m = manager.connect(host=router["ip"],
+                    port=router["port"],
+                    username=router["user"],
+                    password=router["pass"],
                     hostkey_verify=False)
-                    
+
 interface_netconf = m.get_config("running", netconf_filter)
 interface_python = xmltodict.parse(interface_netconf.xml)["rpc-reply"]["data"]
 pprint(interface_python["interfaces"]["interface"]["name"]["#text"])
-    
